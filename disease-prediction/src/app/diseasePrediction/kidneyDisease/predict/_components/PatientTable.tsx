ae938df81cc6b56697b55828970246f855a048d5
Dice <102275981+Dice15@users.noreply.github.com>
2024-08-26 17:03:46 +0900

    Update: 가상 테이블 적용
    1) 검사 결과 출력 과정에서 3~5초간 프리징되는 현상을 해결하기 위해, 가상 테이블 적용

--- disease-prediction/src/app/diseasePrediction/kidneyDisease/predict/_components/PatientTable.tsx
+++ disease-prediction/src/app/diseasePrediction/kidneyDisease/predict/_components/PatientTable.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import styled from "styled-components";
-
+import { TableVirtuoso } from 'react-virtuoso';
+import styled from 'styled-components';
 
 export interface PatientTableProps {
     tableTitle: string;
@@ -9,56 +9,56 @@ export interface PatientTableProps {
     diseasePredictions: number[];
 }
 
-
 export default function PatientTable({ tableTitle, patientData, diseasePredictions }: PatientTableProps) {
-    // render
+    const columns = Object.keys(patientData[0] || {});
+
     return (
         <Wrapper>
-            {patientData.length > 0 &&
+            {patientData.length > 0 && (
                 <>
-                    <Title>
-                        {tableTitle}
-                    </Title>
+                    <Title>{tableTitle}</Title>
                     <Content>
-                        <Table>
-                            <thead>
+                        <TableVirtuoso
+                            data={patientData}
+                            components={{
+                                Table: (props) => {
+                                    const { context, ...restProps } = props;
+                                    return <Table {...restProps} />;
+                                },
+                            }}
+                            fixedHeaderContent={() => (
                                 <tr>
-                                    {Object.keys(patientData[0])
-                                        .map((feature, index) => (
-                                            <th key={index} style={{ border: '1px solid black', padding: '8px', backgroundColor: '#cbcbcb' }}>
-                                                {feature}
-                                            </th>
-                                        ))}
+                                    {columns.map((column, index) => (
+                                        <TableTh key={index} $isFirst={index === 0}>
+                                            {column}
+                                        </TableTh>
+                                    ))}
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {patientData.map((row, rowIndex) => (
-                                    <tr
-                                        key={rowIndex}
-                                        style={{
-                                            backgroundColor: `${(diseasePredictions[rowIndex] === undefined || diseasePredictions[rowIndex] === 2) ? '#f2f2f2' : diseasePredictions[rowIndex] ? '#FFA3A3' : '#89baff'}`
-                                        }}
-                                    >
-                                        {Object.keys(patientData[0])
-                                            .map((feature, colIndex) => (
-                                                <td key={colIndex} style={{ border: '1px solid black', padding: '8px' }}>
-                                                    {feature === 'classification'
-                                                        ? (diseasePredictions[rowIndex] === undefined || diseasePredictions[rowIndex] === 2) ? ' ' : diseasePredictions[rowIndex] ? '신장 질환' : '정상'
-                                                        : row[feature]
-                                                    }
-                                                </td>
-                                            ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </Table>
+                            )}
+                            itemContent={(index, row) => (
+                                <>
+                                    {columns.map((feature, colIndex) => (
+                                        <TableTd
+                                            key={colIndex}
+                                            $isFirst={colIndex === 0}
+                                            $prediction={diseasePredictions[index]}
+                                        >
+                                            {feature === 'classification'
+                                                ? diseasePredictions[index] === undefined || diseasePredictions[index] === 2
+                                                    ? '-'
+                                                    : diseasePredictions[index] ? 'Kidney Disease' : 'Normal'
+                                                : row[feature] || '-'}
+                                        </TableTd>
+                                    ))}
+                                </>
+                            )}
+                        />
                     </Content>
                 </>
-            }
+            )}
         </Wrapper>
     );
 }
-
 
 const Wrapper = styled.div`
     height: 100%;
@@ -72,20 +72,45 @@ const Wrapper = styled.div`
 const Title = styled.h3`
     font-size: 25px;
     font-weight: bold;
-    margin-bottom: 10px;
-    color: #3182F6; 
+    margin-bottom: 20px;
+    color: #3182F6;
 `;
 
 const Content = styled.div`
-    height: calc(100% - 35px);
+    height: calc(100% - 45px - 4px);
+    border: 2px solid #000000;
     width: 100%;
-    overflow: scroll;
+    overflow: auto;
 `;
 
 const Table = styled.table`
-    height:100%;
     width: 100%;
     border-collapse: collapse;
+    table-layout: auto; 
+    text-align: center;
 `;
 
-//width: '100%', maxWidth: '100%', height: 'calc(100% - 20px)', maxHeight: 'calc(100% - 20px)', paddingTop: '20px', overflowY: 'scroll', overflowX: 'scroll'
+const TableTh = styled.th<{ $isFirst: boolean }>`
+    padding: 8px;
+    box-shadow: inset 1px 0 0 0 black;
+    background-color: #cbcbcb;
+ 
+    ${({ $isFirst }) => $isFirst && `
+        box-shadow: none;
+    `}
+`;
+
+const TableTd = styled.td<{ $isFirst: boolean, $prediction: number | undefined }>`
+    user-select: text;
+    padding: 8px;
+    box-shadow: inset 1px 0 0 0 black;
+    background-color: ${({ $prediction }) =>
+        $prediction === undefined || $prediction === 2
+            ? '#f2f2f2'
+            : $prediction ? '#FFA3A3' : '#89baff'
+    };
+
+    ${({ $isFirst }) => $isFirst && `
+        box-shadow: none;
+    `}
+`;
